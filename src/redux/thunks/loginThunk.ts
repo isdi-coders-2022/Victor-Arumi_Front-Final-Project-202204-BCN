b4c3ad50ae7fa5da40c021ae6c614667026cc909
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-01 20:42:29 +0200

    Added login thunk

---
+++ src/redux/thunks/loginThunk.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { ILoginForm, LogInPayload } from "../../types/types";
+import { logInActionCreator } from "../features/userSlice";
+import { AppDispatch } from "../store";
+
+interface APIResponse {
+  token: string;
+}
+
+const loginThunk =
+  (userCredentials: ILoginForm) => async (dispatch: AppDispatch) => {
+    const url: string = `${process.env.REACT_APP_API_URL}user/login`;
+    const {
+      data: { token },
+    } = await axios.post<APIResponse>(url, userCredentials);
+
+    localStorage.setItem("token", token);
+
+    const userDetails: LogInPayload = await jwtDecode(token);
+
+    dispatch(logInActionCreator(userDetails));
+  };
+
+export default loginThunk;
