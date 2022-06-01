52798fd3767708b48dfab8073c0acb5062b173cf
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-01 09:27:29 +0200

    Removed unused imports and ammended Boolean type uppercase

--- src/components/RegisterForm/RegisterForm.test.tsx
+++ src/components/RegisterForm/RegisterForm.test.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  getAllByRole,
-  getByRole,
-  render,
-  screen,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import RegisterForm from "./RegisterForm";
 
