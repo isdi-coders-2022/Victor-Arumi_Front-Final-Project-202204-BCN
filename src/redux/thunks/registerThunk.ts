1c9f880984be93b5a8ac744812e8cd1709310b25
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-02 13:55:21 +0200

    Renamed thunk responseApi interfaces to pascalcase

--- src/redux/thunks/registerThunk.ts
+++ src/redux/thunks/registerThunk.ts
@@ -3,7 +3,7 @@ import { IRegisterForm } from "../../types/types";
 import { registerActionCreator } from "../features/userSlice";
 import { AppDispatch } from "../store";
 
-interface registerApiResponse {
+interface RegisterApiResponse {
   username: string;
 }
 
@@ -12,7 +12,7 @@ const registerThunk =
     const url: string = `${process.env.REACT_APP_API_URL}user/register`;
     const {
       data: { username },
-    } = await axios.post<registerApiResponse>(url, userData);
+    } = await axios.post<RegisterApiResponse>(url, userData);
 
     dispatch(registerActionCreator({ username }));
   };
