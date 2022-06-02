201ed85ca9a0be18a8e8a1dcb1749e9757db1f17
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-02 10:34:24 +0200

    Refactorized register thunk to dispatch only username, test update

--- src/redux/thunks/registerThunk.test.ts
+++ src/redux/thunks/registerThunk.test.ts
@@ -3,23 +3,28 @@ import { server } from "../../mocks/server";
 import { Dispatch } from "@reduxjs/toolkit";
 import registerThunk from "./registerThunk";
 import { mockNewUser } from "../../mocks/handlers";
+import { registerActionCreator } from "../features/userSlice";
 
 beforeEach(() => server.listen());
 afterEach(() => server.resetHandlers);
 afterAll(() => server.close);
 
 describe("Given a registerThunk", () => {
-  describe("When it is called", () => {
-    test("Then it should call dispatch", async () => {
+  describe("When it is called with user register data", () => {
+    test("Then it should call dispatch with a register action", async () => {
       const dispatch: Dispatch = jest.fn();
 
-      const userData = mockNewUser;
+      const payload = { username: mockNewUser.username };
 
-      const thunk = registerThunk(userData);
+      const registerAction = registerActionCreator(payload);
+
+      const userRegisterData = mockNewUser;
+
+      const thunk = registerThunk(userRegisterData);
 
       await thunk(dispatch);
 
-      expect(dispatch).toHaveBeenCalled();
+      expect(dispatch).toHaveBeenCalledWith(registerAction);
     });
   });
 });
