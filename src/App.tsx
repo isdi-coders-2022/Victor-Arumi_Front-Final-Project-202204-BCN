0a135fab5b0ff5b244573377138d57cc9d9aae09
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-03 13:20:17 +0200

    Added navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import AppStyled from "./AppStyled";
+import NavBar from "./components/NavBar/NavBar";
 import ClubsPage from "./pages/ClubsPage/ClubsPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
@@ -9,15 +9,13 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 function App() {
   return (
     <>
-      <AppStyled>
-        <h1 className="text-3xl font-bold underline">PadelBookings</h1>
-        <Routes>
-          <Route path="/" element={<Navigate to="/clubs" />} />
-          <Route path="/clubs" element={<ClubsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </AppStyled>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/clubs" />} />
+        <Route path="/clubs" element={<ClubsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </>
   );
 }
