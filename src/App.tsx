1cc23665eab932743643edd86666f59a7c35051e
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-03 20:32:14 +0200

    Created mockbookings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AppStyled from "./AppStyled";
 import NavBar from "./components/NavBar/NavBar";
 import ClubsPage from "./pages/ClubsPage/ClubsPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
@@ -9,13 +10,15 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/clubs" />} />
-        <Route path="/clubs" element={<ClubsPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
+      <AppStyled>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/clubs" />} />
+          <Route path="/clubs" element={<ClubsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </AppStyled>
     </>
   );
 }
