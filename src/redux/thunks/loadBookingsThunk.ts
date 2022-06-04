60e9d52f3a20f2bae5ad0db163c453eb7faf0acf
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-04 11:01:16 +0200

    Created login thunk

---
+++ src/redux/thunks/loadBookingsThunk.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { IBooking } from "../../types/types";
+import { loadBookingsActionCreator } from "../features/bookingsSlice";
+import { AppDispatch } from "../store";
+
+interface AxiosGetBookingsResponse {
+  bookings: IBooking[];
+}
+
+const loadBookingsThunk = () => async (dispatch: AppDispatch) => {
+  const url: string = `${process.env.REACT_APP_API_URL}bookings`;
+  const {
+    data: { bookings },
+  } = await axios.get<AxiosGetBookingsResponse>(url);
+
+  dispatch(loadBookingsActionCreator(bookings));
+};
+
+export default loadBookingsThunk;
