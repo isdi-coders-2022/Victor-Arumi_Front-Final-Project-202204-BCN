e618b35c4f36f024a3c7a39760111d7c1347eeff
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-05 14:22:55 +0200

    Modified app including loginControl

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AppStyled from "./AppStyled";
+import LoginControl from "./components/LoginControl/LoginControl";
 import NavBar from "./components/NavBar/NavBar";
 import BookingsPage from "./pages/BookingsPage/BookingsPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
@@ -14,8 +15,22 @@ function App() {
         <NavBar />
 
         <Routes>
-          <Route path="/" element={<Navigate to="/bookings" />} />
-          <Route path="/bookings" element={<BookingsPage />} />
+          <Route
+            path="/"
+            element={
+              <LoginControl>
+                <Navigate to="/bookings" />
+              </LoginControl>
+            }
+          />
+          <Route
+            path="/bookings"
+            element={
+              <LoginControl>
+                <BookingsPage />
+              </LoginControl>
+            }
+          />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
