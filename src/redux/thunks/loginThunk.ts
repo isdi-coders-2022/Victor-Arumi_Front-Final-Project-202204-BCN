8dd260ebd484befe9087272f5b927974d1a0bcb0
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-06 11:50:41 +0200

    Added success and error  modals to login

--- src/redux/thunks/loginThunk.ts
+++ src/redux/thunks/loginThunk.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import { toast } from "react-toastify";
 import { ILoginForm, LogInPayload } from "../../types/types";
 import { logInActionCreator } from "../features/userSlice";
 import { AppDispatch } from "../store";
@@ -11,15 +12,20 @@ interface LoginApiResponse {
 const loginThunk =
   (userCredentials: ILoginForm) => async (dispatch: AppDispatch) => {
     const url: string = `${process.env.REACT_APP_API_URL}user/login`;
-    const {
-      data: { token },
-    } = await axios.post<LoginApiResponse>(url, userCredentials);
 
-    localStorage.setItem("token", token);
+    try {
+      const {
+        data: { token },
+      } = await axios.post<LoginApiResponse>(url, userCredentials);
 
-    const userDetails: LogInPayload = await jwtDecode(token);
+      localStorage.setItem("token", token);
 
-    dispatch(logInActionCreator(userDetails));
+      const userDetails: LogInPayload = await jwtDecode(token);
+      dispatch(logInActionCreator(userDetails));
+      toast.success(`!Bienvenido ${userDetails.username}!`);
+    } catch (error: any) {
+      toast.error(`Algo ha salido mal: ${error.response.data.msg}`);
+    }
   };
 
 export default loginThunk;
