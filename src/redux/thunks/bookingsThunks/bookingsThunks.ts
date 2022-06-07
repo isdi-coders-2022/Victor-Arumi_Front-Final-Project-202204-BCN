b5ecf8f4346bacf73a5d6d092efdb2e958320361
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-07 17:40:15 +0200

    Added delete thunk

--- src/redux/thunks/bookingsThunks/bookingsThunks.ts
+++ src/redux/thunks/bookingsThunks/bookingsThunks.ts
@@ -1,19 +1,50 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import { IBooking } from "../../../types/types";
-import { loadBookingsActionCreator } from "../../features/bookingsSlice";
+import {
+  deleteBookingActionCreator,
+  loadBookingsActionCreator,
+} from "../../features/bookingsSlice";
 import { AppDispatch } from "../../store";
 
 interface AxiosGetBookingsResponse {
   bookings: IBooking[];
 }
 
-const loadBookingsThunk = () => async (dispatch: AppDispatch) => {
+export const loadBookingsThunk = () => async (dispatch: AppDispatch) => {
   const url: string = `${process.env.REACT_APP_API_URL}bookings`;
+  const id = toast.loading("Cargando...", {
+    isLoading: true,
+    position: "top-center",
+  });
   const {
     data: { bookings },
   } = await axios.get<AxiosGetBookingsResponse>(url);
-
   dispatch(loadBookingsActionCreator(bookings));
+  toast.update(id, {
+    isLoading: false,
+    autoClose: 300,
+  });
 };
 
-export default loadBookingsThunk;
+export const deleteBookingThunk =
+  (bookingIdToDelete: string) => async (dispatch: AppDispatch) => {
+    const url: string = `${process.env.REACT_APP_API_URL}bookings/${bookingIdToDelete}`;
+    try {
+      const {
+        status,
+        data: { msg },
+      } = await axios.delete(url);
+
+      if (status === 200) {
+        dispatch(deleteBookingActionCreator(bookingIdToDelete));
+        toast.success("Reserva eliminada");
+      }
+
+      if (status === 404) {
+        toast.error(msg);
+      }
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
