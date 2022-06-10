9d3c3569dc515efc2744a8dbf00aa87fed7cc721
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-10 20:13:22 +0200

    Modifyied create booking page with useselector

--- src/pages/CreateBookingPage/CreateBookingPage.tsx
+++ src/pages/CreateBookingPage/CreateBookingPage.tsx
@@ -1,10 +1,25 @@
-import CreateBookingForm from "../../components/CreateBookingForm/CreateBookingForm";
+import BookingForm from "../../components/CreateBookingForm/CreateBookingForm";
+import { useAppSelector } from "../../redux/store/hooks";
 
 const CreateBookingPage = (): JSX.Element => {
+  const { id: userId, username } = useAppSelector((state) => state.user);
+  const usernames = [username ?? ""];
+
+  const newBookingStartingValues = {
+    id: "",
+    club: "",
+    owner: userId ?? "",
+    date: "",
+    hour: "",
+    courtType: "Indoor",
+    players: [userId ?? ""],
+    open: true,
+  };
+
   return (
     <div className="bg-customblue/20">
       <h2 className="text-center my-5 text-3xl">Crear reserva</h2>
-      <CreateBookingForm />
+      <BookingForm booking={newBookingStartingValues} usernames={usernames} />
     </div>
   );
 };
