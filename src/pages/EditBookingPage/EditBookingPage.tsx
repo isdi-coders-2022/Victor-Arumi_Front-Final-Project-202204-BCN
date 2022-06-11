2f488886f0c28210e0e091d1ae3de7aa7c50c327
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-11 16:14:16 +0200

    Renamed booking form

--- src/pages/EditBookingPage/EditBookingPage.tsx
+++ src/pages/EditBookingPage/EditBookingPage.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import BookingForm from "../../components/CreateBookingForm/CreateBookingForm";
+import BookingForm from "../../components/BookingForm/BookingForm";
 import { useAppSelector } from "../../redux/store/hooks";
 
 const EditBookingPage = (): JSX.Element => {
