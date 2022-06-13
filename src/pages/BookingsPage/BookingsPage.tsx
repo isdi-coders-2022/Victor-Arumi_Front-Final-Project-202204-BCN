974c7cc03bfe100be75f7d114f55ba16d63a83df
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-13 16:03:35 +0200

    Added styled component page

--- src/pages/BookingsPage/BookingsPage.tsx
+++ src/pages/BookingsPage/BookingsPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BookingsList from "../../components/BookingsList/BookingsList";
 import { useAppDispatch, useAppSelector } from "../../redux/store/hooks";
 import { loadBookingsThunk } from "../../redux/thunks/bookingsThunks/bookingsThunks";
+import BookingsPageStyled from "./BookingsPageStyled";
 
 const BookingsPage = (): JSX.Element => {
   const dispatch = useAppDispatch();
@@ -27,30 +28,42 @@ const BookingsPage = (): JSX.Element => {
   };
 
   return (
-    <div className="bg-customblue/20 h-full flex flex-col items-center ">
-      <h2 className="text-center my-5 text-3xl">Reservas</h2>
-      {emptyPage ? (
-        <h3 className="text-center my-5 text-2xl">No hay más reservas</h3>
-      ) : null}
-      <BookingsList bookings={bookings} />
-      <div className="flex items-center text-center w-80 mb-6 justify-around ">
-        <button
-          className="disabled:opacity-50"
-          onClick={previousPage}
-          disabled={firstPage}
-        >
-          Anterior
-        </button>
-        <p>Página {page}</p>
-        <button
-          className="disabled:opacity-50"
-          onClick={nextPage}
-          disabled={lastPage}
-        >
-          Siguiente
-        </button>
+    <BookingsPageStyled>
+      <div className=" flex flex-col items-center ">
+        <h2 className="text-center my-5 text-3xl">Reservas</h2>
+        {emptyPage ? (
+          <h3 className="text-center my-5 text-2xl">No hay más reservas</h3>
+        ) : null}
+        <div className="filters-container">
+          <div className="filters-container__section">
+            <button className="filters-container__button">Indoor</button>
+            <button className="filters-container__button">Outdoor</button>
+          </div>
+          <div className="filters-container__section">
+            <button className="filters-container__button">Res. Abierta</button>
+            <input type="date" className="filters-container__button"></input>
+          </div>
+        </div>
+        <BookingsList bookings={bookings} />
+        <div className="flex items-center text-center w-80 mb-6 justify-around ">
+          <button
+            className="disabled:opacity-50"
+            onClick={previousPage}
+            disabled={firstPage}
+          >
+            Anterior
+          </button>
+          <p>Página {page}</p>
+          <button
+            className="disabled:opacity-50"
+            onClick={nextPage}
+            disabled={lastPage}
+          >
+            Siguiente
+          </button>
+        </div>
       </div>
-    </div>
+    </BookingsPageStyled>
   );
 };
 
---
+++ src/pages/BookingsPage/BookingsPageStyled.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const BookingsPageStyled = styled.div`
+  background-color: rgb(25, 116, 182, 0.2);
+  height: 100%;
+
+  .filters-container {
+    border: 1px solid red;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-bottom: 30px;
+
+    &__section {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      margin-left: 8px;
+    }
+
+    &__button {
+      width: 150px;
+      height: 40px;
+      background-color: rgb(25, 116, 182, 0.2);
+      margin: 5px 0px;
+      border: 1px solid rgb(25, 116, 182);
+      border-radius: 1.5rem;
+    }
+
+    @media (min-width: 710px) {
+      margin-bottom: 50px;
+      .filters-container__section {
+        flex-direction: row;
+        width: 100%;
+      }
+    }
+  }
+`;
+
+export default BookingsPageStyled;
