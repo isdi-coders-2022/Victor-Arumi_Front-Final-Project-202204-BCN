f1b3988cfe441b648c7f888df7ab9252d828fa08
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-13 10:21:00 +0200

    Refactorized pagination corner cases

--- src/pages/BookingsPage/BookingsPage.tsx
+++ src/pages/BookingsPage/BookingsPage.tsx
@@ -6,7 +6,7 @@ import { loadBookingsThunk } from "../../redux/thunks/bookingsThunks/bookingsThu
 const BookingsPage = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const bookings = useAppSelector((state) => state.bookings);
-  const limit = 8;
+  const limit = 4;
   const initialPage = 1;
 
   const [page, setPage] = useState(initialPage);
@@ -15,25 +15,40 @@ const BookingsPage = (): JSX.Element => {
     dispatch(loadBookingsThunk(limit, page));
   }, [dispatch, page]);
 
+  const emptyPage = bookings.length === 0;
+  const lastPage = bookings.length < limit;
+  const firstPage = page === 1;
+
   const nextPage = () => {
-    if (bookings.length > 0) {
-      setPage(page + 1);
-    }
+    setPage(page + 1);
   };
   const previousPage = () => {
-    if (page > 1) {
-      setPage(page - 1);
-    }
+    setPage(page - 1);
   };
 
   return (
     <div className="bg-customblue/20 h-full flex flex-col items-center ">
       <h2 className="text-center my-5 text-3xl">Reservas</h2>
+      {emptyPage ? (
+        <h3 className="text-center my-5 text-2xl">No hay más reservas</h3>
+      ) : null}
       <BookingsList bookings={bookings} />
       <div className="flex items-center text-center w-80 mb-6 justify-around ">
-        <button onClick={previousPage}>Anterior</button>
+        <button
+          className="disabled:opacity-50"
+          onClick={previousPage}
+          disabled={firstPage}
+        >
+          Anterior
+        </button>
         <p>Página {page}</p>
-        <button onClick={nextPage}>Siguiente</button>
+        <button
+          className="disabled:opacity-50"
+          onClick={nextPage}
+          disabled={lastPage}
+        >
+          Siguiente
+        </button>
       </div>
     </div>
   );
