4df77ccd94be6af2bc4b1dc304d669f685baf112
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-15 07:45:21 +0200

    Modified test description to be more accurate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -29,12 +29,12 @@ Object.defineProperty(window, "localStorage", {
 });
 
 describe("Given an App component", () => {
-  describe("When its invoked with a Login Page and with a real token already in the local storage", () => {
+  describe("When its invoked with a Login Page and with a real token (real user Paula Badosa) already in the local storage", () => {
     saveToStorage(
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYTg5NGMzMTVjMDJjZTFjNDM2Mzk5YSIsIm5hbWUiOiJwYXVsYSIsInVzZXJuYW1lIjoiUGF1bGEgQmFkb3NhIiwicHJvZmlsZVBpY3R1cmUiOiIxNjU1MjE1Mjk4NTE4QmFkb3NhLndlYnAiLCJwcm9maWxlUGljdHVyZUJhY2t1cCI6Imh0dHBzOi8vZmlyZWJhc2VzdG9yYWdlLmdvb2dsZWFwaXMuY29tL3YwL2IvcGFkZWxib29raW5ncy03NTE1Mi5hcHBzcG90LmNvbS9vLzE2NTUyMTUyOTg1MThCYWRvc2Eud2VicD9hbHQ9bWVkaWEmdG9rZW49NjdmYTM5NDEtN2MyYi00ODFmLTgyYmEtYTk4YjhmMjczZjdlIiwiaWF0IjoxNjU1MjM5MTE5fQ.Z_b3qaz8xDZu6bjkzPXLOPLce96_NISe_LI0JXE_ZeY"
     );
 
-    test("Then it should render with a token in its local storage and dispatch a login action that will decrypt the token with the user information and that data will be in the store", () => {
+    test("Then it should render with a token in its local storage and dispatch a login action that will decrypt the token with the user information, which will be in the user state of the store", () => {
       const expectedTokenData = {
         booking: {
           booking: {
