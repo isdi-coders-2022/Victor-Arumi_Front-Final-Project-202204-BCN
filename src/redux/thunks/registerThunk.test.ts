f7b0c9930957ac7960a95fdd92a34a9104c425f2
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-23 13:07:43 +0200

    Testes unhappy path

--- src/redux/thunks/registerThunk.test.ts
+++ src/redux/thunks/registerThunk.test.ts
@@ -10,8 +10,26 @@ afterEach(() => server.resetHandlers);
 afterAll(() => server.close);
 
 describe("Given a registerThunk", () => {
-  describe("When it is called with user register data", () => {
+  describe("When it is called with valid user register data", () => {
     test("Then it should call dispatch with a register action", async () => {
+      const dispatch: Dispatch = jest.fn();
+
+      const payload = { username: mockNewUser.username };
+
+      const registerAction = registerActionCreator(payload);
+
+      const userRegisterData = mockNewUser;
+
+      const thunk = registerThunk(userRegisterData);
+
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(registerAction);
+    });
+  });
+
+  describe("When it is called with invalid user register data", () => {
+    test("Then it should return a error message", async () => {
       const dispatch: Dispatch = jest.fn();
 
       const payload = { username: mockNewUser.username };
